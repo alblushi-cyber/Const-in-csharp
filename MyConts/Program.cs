98a383af6c50d948dac7716112924fcb3f6de9f0
mazin <mazin@MAZEN>
2020-12-04 10:37:10 +0400

    Add project files.

---
+++ MyConts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Months = 12;
+            const int Days = 365;
+            const int Weeks = 52;
+
+
+            Console.WriteLine(Months);
+            Console.WriteLine(Days);
+            Console.WriteLine(Weeks);
+        }
+    }
+}
